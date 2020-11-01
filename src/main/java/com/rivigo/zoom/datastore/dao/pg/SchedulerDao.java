ec59125463f5693261c7527a7f2a8b3b6a9c9e3e
Vamsi <vamsi.hemadribhotla@rivigo.com>
2020-11-01 17:45:36 +0530

    add methods in dao

--- src/main/java/com/rivigo/zoom/datastore/dao/pg/SchedulerDao.java
+++ src/main/java/com/rivigo/zoom/datastore/dao/pg/SchedulerDao.java
@@ -4,6 +4,7 @@ import com.rivigo.zoom.datastore.model.Schedule;
 import java.util.Optional;
 import org.jdbi.v3.sqlobject.config.RegisterFieldMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 @RegisterFieldMapper(Schedule.class)
@@ -12,4 +13,15 @@ public interface SchedulerDao extends AbstractDao<Schedule> {
   @SqlQuery(
       "select " + "id " + "name " + "last_successful_run" + "from schedules where name = :name")
   Optional<Schedule> findByName(@Bind("name") String name);
+
+  @SqlQuery("" + "")
+  @GetGeneratedKeys("id")
+  Long add(Schedule schedule);
+
+  @SqlQuery("")
+  @GetGeneratedKeys("id")
+  Long update(Schedule schedule);
+
+  @SqlQuery("" + "")
+  Optional<Schedule> findById(Long id);
 }
--- src/main/java/com/rivigo/zoom/datastore/dao/pg/VideoDao.java
+++ src/main/java/com/rivigo/zoom/datastore/dao/pg/VideoDao.java
@@ -1,7 +1,22 @@
 package com.rivigo.zoom.datastore.dao.pg;
 
 import com.rivigo.zoom.datastore.model.Video;
+import java.util.Optional;
 import org.jdbi.v3.sqlobject.config.RegisterFieldMapper;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 @RegisterFieldMapper(Video.class)
-public interface VideoDao extends AbstractDao<Video> {}
+public interface VideoDao extends AbstractDao<Video> {
+
+  @SqlQuery("" + "")
+  @GetGeneratedKeys("id")
+  Long add(Video video);
+
+  @SqlQuery("")
+  @GetGeneratedKeys("id")
+  Long update(Video video);
+
+  @SqlQuery("" + "")
+  Optional<Video> findById(Long id);
+}
